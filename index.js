ccddd2c2773b3dfd1c0cbff1a188b9cf5ecd858f
Meche221 <binaymeche22@gmail.com>
2025-08-14 15:28:04 +0545

    gsap added

---
+++ index.js
@@ -0,0 +1,23 @@
+gsap.from("#img1", {
+    opacity: 0,
+    delay: 0.4,
+    y: 60,
+    duration:1, 
+})
+gsap.from("#img2", {
+    opacity: 0,
+    delay: 0.4,
+    x: 60,
+    duration:1, 
+})
+gsap.from("#img3", {
+    opacity: 0,
+    delay: 0.4,
+    y: -60,
+    duration:1, 
+})
+gsap.from("#main h1", {
+    opacity: 0,
+    delay: 0.4,
+    duration:1, 
+})
